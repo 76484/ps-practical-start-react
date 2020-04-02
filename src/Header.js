c4edbbefd158337d6f900ffa86ad7ad029890b77
Steven Smith <steven.76484@gmail.com>
2020-04-02 08:19:03 -0400

    Adding Header component

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => (
+  <header className="row">
+    <div className="row">
+      <img alt="logo" className="logo" />
+    </div>
+    <div className="col-md-7 mt-5 subtitle">Providing houses world wide</div>
+  </header>
+);

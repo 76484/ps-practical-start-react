8119917f2982f5f0b34c4f6e34949b835fadf4b0
Steven Smith <steven.76484@gmail.com>
2020-04-02 08:22:05 -0400

    Adding subtitle prop to Header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from "./Header";
 function App() {
   return (
     <div className="container">
-      <Header />
+      <Header subtitle="Providing houses world wide" />
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-export default () => (
+export default ({ subtitle }) => (
   <header className="row">
     <div className="row">
       <img alt="logo" className="logo" />
     </div>
-    <div className="col-md-7 mt-5 subtitle">Providing houses world wide</div>
+    <div className="col-md-7 mt-5 subtitle">{subtitle}</div>
   </header>
 );
